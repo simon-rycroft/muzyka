7df0acf00a205bf5b18f0402b825e8641fed82b1
Simon Rycroft <simon.rycroft@gmail.com>
2015-09-04 13:09:06 +0100

    Added linting with jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ require('babel/register');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
 gulp.task('test', function() {
     return gulp.src(['tests/**/*.js'], { read: false })
@@ -12,5 +13,10 @@ gulp.task('test', function() {
 
 gulp.task('watch-test', function() {
     gulp.watch(['src/**', 'tests/**'], ['test']);
+});
+
+gulp.task('lint', function(){
+    return gulp.src(['src/**'])
+        .pipe(jshint());
 });
 
